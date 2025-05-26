8a3cd478909908f43d31f64d55eaaf6e19cffff1
Alexander Budik <alexander.budik@gmx.at>
2025-05-26 15:27:55 +0200

    Implement exscan 1,2,3

--- exscan-student-1.0.0-Source/src/exscan.c
+++ exscan-student-1.0.0-Source/src/exscan.c
@@ -9,13 +9,105 @@
 
 
 void exscan1(int A[], int n) {
-	// implement an efficient sequential algorithm
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        int temp = A[i];
+        A[i] = sum;
+        sum += temp;
+    }
+}
+
+void up_sweep(int A[], int n) {
+    for (int step = 1; step < n; step *= 2) {
+        for (int i = (n / (2 * step)) - 1; i >= 0; i--) {
+            int index = (2 * i + 1) * step - 1;
+            A[index + step] += A[index];
+        }
+    }
+}
+
+void down_sweep(int A[], int n) {
+    A[n - 1] = 0;
+
+    for (int step = n / 2; step >= 1; step /= 2) {
+        for (int i = 0; i < n / (2 * step); i++) {
+            int left = (2 * i + 1) * step - 1;
+            int right = left + step;
+
+            int temp = A[left];
+            A[left] = A[right];
+            A[right] += temp;
+        }
+    }
 }
 
 void exscan2(int A[], int n) {
-	// implement a recursive sequential algorithm like scan above
+    if (n == 0) return;
+
+    int pow2 = 1;
+    while (pow2 < n) pow2 *= 2;
+
+    int *B = calloc(pow2, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        B[i] = A[i];
+    }
+
+    up_sweep(B, pow2);
+    down_sweep(B, pow2);
+
+    for (int i = 0; i < n; i++) {
+        A[i] = B[i];
+    }
+
+    free(B);
+}
+
+void up_sweep_parallel(int A[], int n) {
+    for (int step = 1; step < n; step *= 2) {
+        #pragma omp parallel for
+        for (int i = 0; i < n / (2 * step); i++) {
+            int index = (2 * i + 1) * step - 1;
+            A[index + step] += A[index];
+        }
+    }
+}
+
+void down_sweep_parallel(int A[], int n) {
+    A[n - 1] = 0;
+
+    for (int step = n / 2; step >= 1; step /= 2) {
+        #pragma omp parallel for
+        for (int i = 0; i < n / (2 * step); i++) {
+            int left = (2 * i + 1) * step - 1;
+            int right = left + step;
+
+            int temp = A[left];
+            A[left] = A[right];
+            A[right] += temp;
+        }
+    }
 }
 
 void exscan3(int A[], int n) {
-	// parallelize the exscan2 function above
+    if (n == 0) return;
+
+    int pow2 = 1;
+    while (pow2 < n) pow2 *= 2;
+
+    int *B = calloc(pow2, sizeof(int));
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        B[i] = A[i];
+    }
+
+    up_sweep_parallel(B, pow2);
+    down_sweep_parallel(B, pow2);
+
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        A[i] = B[i];
+    }
+
+    free(B);
 }
