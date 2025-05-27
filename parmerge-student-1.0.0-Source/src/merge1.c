00d40d8fef327c0f529c22347f485e42a5cf08d8
Alexander Budik <alexander.budik@gmx.at>
2025-05-27 18:50:44 +0200

    Implement merge1.c

--- parmerge-student-1.0.0-Source/src/merge1.c
+++ parmerge-student-1.0.0-Source/src/merge1.c
@@ -11,11 +11,31 @@
 
 #include "merge.h"
 
+int rank(double x, double X[], int n) {
+    int low = 0, high = n;
+    while (low < high) {
+        int mid = (low + high) / 2;
+        if (X[mid] < x) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return low;
+}
+
 void merge(double a[], long n, double b[], long m, double c[]) {
+    #pragma omp parallel for
+    for (long i = 0; i < n; i++) {
+        int r = rank(a[i], b, m);
+        c[i + r] = a[i];
+    }
 
-  // replace this by a parallel merge algorithm
-  seq_merge1(a, n, b, m, c);
-
+    #pragma omp parallel for
+    for (long j = 0; j < m; j++) {
+        int r = rank(b[j], a, n);
+        c[j + r] = b[j];
+    }
 }
 
 
